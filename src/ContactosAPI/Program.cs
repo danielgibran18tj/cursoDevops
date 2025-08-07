6f293e33e754395a7d7e997c66875e27c4386e82
Daniel Hinostrosa <dhinostrosa@viamatica.com>
2025-08-06 23:52:58 -0500

    Se dockeriza app

--- src/ContactosAPI/Program.cs
+++ src/ContactosAPI/Program.cs
@@ -2,15 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
